ef2902b46dd057fbd5ec18367703be879e7a145e
Natepalm <89954085+audiophonicz@users.noreply.github.com>
2025-09-25 13:42:36 -0400

    Update honeypot_repo_setup.ps1
    fixed example

--- Honeypot Repository Scripts/Windows Honeypot Repository/honeypot_repo_setup.ps1
+++ Honeypot Repository Scripts/Windows Honeypot Repository/honeypot_repo_setup.ps1
@@ -4,7 +4,7 @@
 .PARAMETER Folder
     The folder to set the SACLs on
 .EXAMPLE
-    .\honeypot_repo_setup.ps1 R:\backups
+    .\honeypot_repo_setup.ps1 <REPO_PATH>
 .NOTES
     - Run as Administrator.
 #>
@@ -78,6 +78,7 @@ $adminRule = New-Object System.Security.AccessControl.FileSystemAuditRule (
 $acl.AddAuditRule($everyoneRule)
 $acl.AddAuditRule($adminRule)
 Set-Acl -Path $Folder -AclObject $acl
+
 
 
 
